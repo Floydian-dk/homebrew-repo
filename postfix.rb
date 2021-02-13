18ea06d275550fb844130ee464ee4518f4b55c0d
Floydian-dk <webmaster@floyd.dk>
2021-02-13 18:25:22 +0100

    Another Daemon folder fix.

--- postfix.rb
+++ postfix.rb
@@ -19,7 +19,7 @@ class Postfix < Formula
       s.gsub! "$install_root$queue_directory", "$queue_directory"
       s.gsub! "$install_root$meta_directory", "$meta_directory"
       s.gsub! "$install_root$config_directory", "$config_directory"
-      s.gsub! "$install_root$daemon_directory", "$daemon_directory"
+      #s.gsub! "$install_root$daemon_directory", "$daemon_directory"
     end
 
     ccargs = %W[
@@ -112,7 +112,7 @@ class Postfix < Formula
         sendmail_path=/sbin/sendmail
         setgid_group=_postdrop
         shlib_directory=/lib/postfix
-        daemon_directory=#{libexec}/postfix
+        daemon_directory=/libexec/postfix
         command_directory=/sbin
         manpage_directory=/share/man
         meta_directory=#{etc}/postfix
