5ca5417ffd4e27b7d5be4603b014bc17dd628c62
Floydian-dk <webmaster@floyd.dk>
2021-02-14 00:40:52 +0100

    First test of ccargs

--- postfix.rb
+++ postfix.rb
@@ -71,10 +71,13 @@ class Postfix < Formula
     "-f",
     "Makefile.init",
     "makefiles",
+    "CCARGS='",
+    "-DUSE_SASL_AUTH",
+    "-DDEF_SERVER_SASL_TYPE=\\\"dovecot\\\""
     *ccargs,
     *auxlibspcre,
     *auxlibsmysql
-#    "CCARGS=\"-DUSE_SASL_AUTH -DDEF_SERVER_SASL_TYPE=dovecot -DDEF_COMMAND_DIR=/usr/local/sbin -DDEF_CONFIG_DIR=/usr/local/etc/postfix -DDEF_DAEMON_DIR=/usr/local/libexec/postfix -DUSE_TLS -DHAS_PCRE -I/usr/local/include -DHAS_SSL -I/usr/local/Cellar/openssl@1.1/1.1.1i/include -DHAS_MYSQL -I/usr/local/Cellar/mariadb@10.4/10.4.17/include/mysql\"",
+#    "CCARGS=\" -DDEF_SERVER_SASL_TYPE=dovecot -DDEF_COMMAND_DIR=/usr/local/sbin -DDEF_CONFIG_DIR=/usr/local/etc/postfix -DDEF_DAEMON_DIR=/usr/local/libexec/postfix -DUSE_TLS -DHAS_PCRE -I/usr/local/include -DHAS_SSL -I/usr/local/Cellar/openssl@1.1/1.1.1i/include -DHAS_MYSQL -I/usr/local/Cellar/mariadb@10.4/10.4.17/include/mysql\"",
 #    "AUXLIBS_PCRE=-L/usr/local/lib -lpcre",
 #    "AUXLIBS_MYSQL=-L/usr/local/opt/mariadb@10.4/lib -R/usr/local/opt/mariadb@10.4/lib -lmysqlclient -lz -lm"
 
