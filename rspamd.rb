a978ac93b179744f24f4d3fc389a4be81d107a0c
Floydian-dk <webmaster@floyd.dk>
2021-02-16 08:19:27 +0100

    Rspamd: Escaping dash.

--- rspamd.rb
+++ rspamd.rb
@@ -51,7 +51,7 @@ class Rspamd < Formula
 
     args2 = %W[
       -G
-      \"CodeBlocks\ -\ Unix\ Makefiles\"
+      \"CodeBlocks\ \-\ Unix\ Makefiles\"
       -DCMAKE_BUILD_TYPE=MacPorts
       -DCMAKE_INSTALL_PREFIX=\"#{prefix}\"
       -DCMAKE_INSTALL_NAME_DIR=\"/usr/local/lib\"
