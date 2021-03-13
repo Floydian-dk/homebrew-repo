33e5636c5d0e118b10e038d773bdb5c728b44a66
Floydian-dk <webmaster@floyd.dk>
2021-03-13 10:37:33 +0100

    RSPAMD: Cleaning up - investigating dependency issues.

--- rspamd.rb
+++ rspamd.rb
@@ -5,33 +5,32 @@ class Rspamd < Formula
   sha256 "002aee47dc4d6f8c6c0572b58ccb0cbcbb9bb7689442c33a5a5cf893e72506db"
   license "Apache-2"
 
-  depends_on "pkg-config"
-  depends_on "libunwind-headers"
-  depends_on "llvm"
-  depends_on "libsodium"
+  #depends_on "pkg-config"
+  #depends_on "libunwind-headers"
+  #depends_on "llvm"
+  #depends_on "libsodium"
 
-  depends_on "openssl@1.1"
-  depends_on "icu4c"
-  depends_on "glib"
-  depends_on "ragel"
-  depends_on "lua"
-  depends_on "luajit"
-  depends_on "sqlite"
-  depends_on "libmagic"
-  depends_on "pcre"
-  depends_on "hyperscan"
-  depends_on "zlib"
-  depends_on "hiredis"
-  depends_on "redis"
-  depends_on "perl"
-  depends_on "cmake" => :build
+  #depends_on "openssl@1.1"
+  #depends_on "icu4c"
+  #depends_on "glib"
+  #depends_on "ragel"
+  #depends_on "lua"
+  #depends_on "luajit"
+  #depends_on "sqlite"
+  #depends_on "libmagic"
+  #depends_on "pcre"
+  #depends_on "hyperscan"
+  #depends_on "zlib"
+  #depends_on "hiredis"
+  #depends_on "redis"
+  #depends_on "perl"
+  #depends_on "cmake" => :build
 
   def install
     #ENV["CMAKE_GENERATOR"] = "CodeBlocks - Unix Makefiles"
     ENV["PATH"] = "/usr/local/Homebrew/Library/Homebrew/shims/mac/super:/usr/local/opt/cmake/bin:/opt/local/bin:/opt/local/sbin:/usr/bin:/bin:/usr/sbin:/sbin"
     ENV["CC"] = "/usr/bin/clang"
     ENV["CXX"] = "/usr/bin/clang++"
-    ENV["CC_PRINT_OPTIONS"] = "YES"
     #ENV["CC_PRINT_OPTIONS_FILE"] = "'#{buildpath}/.CC_PRINT_OPTIONS'"
     ENV["CFLAGS"] = "-pipe -Os -DNDEBUG -I/opt/local/include -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk"
     ENV["CPATH"] = "/opt/local/include"
@@ -80,9 +79,6 @@ class Rspamd < Formula
       -DENABLE_LIBUNWIND=ON
       -DENABLE_HYPERSCAN=ON
       -DENABLE_FANN=ON
-      -DENABLE_GD=ON
-      -DENABLE_TORCH=ON
-      -DINSTALL_EXAMPLES=ON
       -DLIBDIR=#{lib}
       -DLOGDIR=/var/log/rspamd
       -DMANDIR=#{man}
@@ -91,65 +87,11 @@ class Rspamd < Formula
       -DRUNDIR=#{var}/run/rspamd
       -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15
       -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk
-      -DCC_PRINT_OPTIONS=YES
       -DENABLE_FULL_DEBUG=ON
     ]
 #    -DENABLE_LIBUNWIND=ON
 #    -DENABLE_LUA_REPL=ON
 
-
-    args2 = %W[
-      -G\"CodeBlocks - Unix Makefiles\"
-      -DCC=/usr/bin/clang
-      -DCC_PRINT_OPTIONS=YES
-      -DCFLAGS=\"-pipe -Os -DNDEBUG -I/opt/local/include -isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk\"
-      -DCPATH=\"/usr/local/include\"
-      -DCPPFLAGS=\"-isysroot/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk\"
-      -DDEVELOPER_DIR=\"/Library/Developer/CommandLineTools\"
-      -DF90FLAGS=\"-pipe -Os -m64\"
-      -DFCFLAGS=\"-pipe -Os -m64\"
-      -DFFLAGS=\"-pipe -Os -m64\"
-      -DOBJCXX=\"/usr/bin/clang++\"
-      -DCMAKE_BUILD_TYPE=Brew
-      -DCMAKE_INSTALL_PREFIX=\"#{prefix}\"
-      -DCMAKE_INSTALL_NAME_DIR=\"#{lib}\"
-      -DCMAKE_SYSTEM_PREFIX_PATH=\"/usr/local;/usr\"
-      -DCMAKE_C_COMPILER=/usr/bin/clang
-      -DCMAKE_CXX_COMPILER=/usr/bin/clang++
-      -DCMAKE_POLICY_DEFAULT_CMP0025=NEW
-      -DCMAKE_POLICY_DEFAULT_CMP0060=NEW
-      -DCMAKE_VERBOSE_MAKEFILE=ON
-      -DCMAKE_COLOR_MAKEFILE=ON
-      -DCMAKE_FIND_FRAMEWORK=LAST
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-      -DCMAKE_MAKE_PROGRAM=/usr/bin/make
-      -DCMAKE_MODULE_PATH=\"/opt/local/share/cmake/Modules\"
-      -DCMAKE_PREFIX_PATH=\"/opt/local/share/cmake/Modules\"
-      -DCMAKE_BUILD_WITH_INSTALL_RPATH:BOOL=ON
-      -DCMAKE_INSTALL_RPATH=\"#{lib}\"
-      -Wno-dev
-      -DCMAKE_EXPORT_COMPILE_COMMANDS=ON
-      -DCMAKE_INSTALL_PREFIX=/usr/local
-      -DCONFDIR=/usr/local/etc/rspamd
-      -DDBDIR=/usr/local/var/lib/rspamd
-      -DENABLE_FANN=ON
-      -DENABLE_GD=ON
-      -DENABLE_HYPERSCAN=ON
-      -DENABLE_LIBUNWIND=ON
-      -DENABLE_LUAJIT=ON
-      -DENABLE_SNOWBALL=ON
-      -DENABLE_TORCH=ON
-      -DINSTALL_EXAMPLES=ON
-      -DLIBDIR=/usr/local/lib
-      -DLOGDIR=/usr/local/var/log/rspamd
-      -DMANDIR=/usr/local/share/man
-      -DNO_SHARED=ON
-      -DPCRE_ROOT_DIR=/usr/lib
-      -DRSPAMD_USER=_rspamd
-      -DRUNDIR=/usr/local/var/run/rspamd
-      -DCMAKE_OSX_DEPLOYMENT_TARGET=\"10.15\"
-      -DCMAKE_OSX_SYSROOT=/Library/Developer/CommandLineTools/SDKs/MacOSX10.15.sdk
-    ]
 
     #system "mkdir rspamd.build"
     #system "cd rspamd.build"
