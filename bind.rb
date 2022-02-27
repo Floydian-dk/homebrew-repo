3dc664f2df9c71b9efadfddfbd752fca85a4c65c
Floydian-dk <webmaster@floyd.dk>
2022-02-27 13:42:52 +0100

    Bind: Upgrading to v9.16.26 Various fixes.

--- bind.rb
+++ bind.rb
@@ -8,8 +8,8 @@ class Bind < Formula
   # "version_scheme" because someone upgraded to 9.15.0, and required a
   # downgrade.
 
-  url "https://downloads.isc.org/isc/bind9/9.16.12/bind-9.16.12.tar.xz"
-  sha256 "9914af9311fd349cab441097898d94fb28d0bfd9bf6ed04fe1f97f042644da7f"
+  url "https://downloads.isc.org/isc/bind9/9.16.26/bind-9.16.26.tar.xz"
+  sha256 "70b39a5eb71650358ec9ba41da3050d32aeac0aeb4a466684b23f35affa7fb45"
   license "MPL-2.0"
   version_scheme 1
   head "https://gitlab.isc.org/isc-projects/bind9.git"
@@ -26,7 +26,7 @@ class Bind < Formula
   depends_on "libidn2"
   depends_on "libuv"
   depends_on "openssl@1.1"
-  depends_on "python@3.9"
+  depends_on "python@3.10"
 
   resource "ply" do
     url "https://files.pythonhosted.org/packages/e5/69/882ee5c9d017149285cab114ebeab373308ef0f874fcdac9beb90e0ac4da/ply-3.11.tar.gz"
@@ -34,12 +34,12 @@ class Bind < Formula
   end
 
   def install
-    xy = Language::Python.major_minor_version Formula["python@3.9"].opt_bin/"python3"
+    xy = Language::Python.major_minor_version Formula["python@3.10"].opt_bin/"python3"
     vendor_site_packages = libexec/"vendor/lib/python#{xy}/site-packages"
     ENV.prepend_create_path "PYTHONPATH", vendor_site_packages
     resources.each do |r|
       r.stage do
-        system Formula["python@3.9"].opt_bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
+        system Formula["python@3.10"].opt_bin/"python3", *Language::Python.setup_install_args(libexec/"vendor")
       end
     end
 
@@ -48,11 +48,12 @@ class Bind < Formula
 
     args = [
       "--prefix=#{prefix}",
+      "--sysconfdir=#{pkgetc}",
       "--with-json-c",
       "--with-openssl=#{Formula["openssl@1.1"].opt_prefix}",
       "--with-libjson=#{Formula["json-c"].opt_prefix}",
       "--with-python-install-dir=#{vendor_site_packages}",
-      "--with-python=#{Formula["python@3.9"].opt_bin}/python3",
+      "--with-python=#{Formula["python@3.10"].opt_bin}/python3",
       "--without-lmdb",
       "--with-libidn2=#{Formula["libidn2"].opt_prefix}",
       "--infodir=#{info}",
@@ -61,9 +62,6 @@ class Bind < Formula
       "--mandir=#{man}",
 
     ]
-    on_linux do
-      args << "--disable-linux-caps"
-    end
     system "./configure", *args
 
     system "make"
@@ -71,110 +69,28 @@ class Bind < Formula
 
     (buildpath/"named.conf").write named_conf
     system "#{sbin}/rndc-confgen", "-a", "-c", "#{buildpath}/rndc.key"
-    etc.install "named.conf", "rndc.key"
+    pkgetc.install "named.conf", "rndc.key"
   end
 
   def post_install
     (var/"log/named").mkpath
-
-    # Create initial configuration/zone/ca files.
-    # (Mirrors Apple system install from 10.8)
-    unless (var/"named").exist?
-      (var/"named").mkpath
-      (var/"named/localhost.zone").write localhost_zone
-      (var/"named/named.local").write named_local
-    end
+    (var/"named").mkpath
   end
 
   def named_conf
     <<~EOS
-      //
-      // Include keys file
-      //
-      include "#{etc}/rndc.key";
-
-      // Declares control channels to be used by the rndc utility.
-      //
-      // It is recommended that 127.0.0.1 be the only address used.
-      // This also allows non-privileged users on the local host to manage
-      // your name server.
-
-      //
-      // Default controls
-      //
-      controls {
-          inet 127.0.0.1 port 54 allow { any; }
-          keys { "rndc-key"; };
-      };
+    logging {
+         category default {
+             _default_log;
+         };
+         channel _default_log {
+             file "#{var}/log/named/named.log" versions 10 size 1m;
+             severity info;
+             print-time yes;      };
 
       options {
           directory "#{var}/named";
-          /*
-           * If there is a firewall between you and nameservers you want
-           * to talk to, you might need to uncomment the query-source
-           * directive below.  Previous versions of BIND always asked
-           * questions using port 53, but BIND 8.1 uses an unprivileged
-           * port by default.
-           */
-          // query-source address * port 53;
       };
-      //
-      // a caching only nameserver config
-      //
-      zone "localhost" IN {
-          type master;
-          file "localhost.zone";
-          allow-update { none; };
-      };
-
-      zone "0.0.127.in-addr.arpa" IN {
-          type master;
-          file "named.local";
-          allow-update { none; };
-      };
-
-      logging {
-              category default {
-                      _default_log;
-              };
-
-              channel _default_log  {
-                      file "#{var}/log/named/named.log";
-                      severity info;
-                      print-time yes;
-              };
-      };
-    EOS
-  end
-
-  def localhost_zone
-    <<~EOS
-      $TTL    86400
-      $ORIGIN localhost.
-      @            1D IN SOA    @ root (
-                          42        ; serial (d. adams)
-                          3H        ; refresh
-                          15M        ; retry
-                          1W        ; expiry
-                          1D )        ; minimum
-
-                  1D IN NS    @
-                  1D IN A        127.0.0.1
-    EOS
-  end
-
-  def named_local
-    <<~EOS
-      $TTL    86400
-      @       IN      SOA     localhost. root.localhost.  (
-                                            1997022700 ; Serial
-                                            28800      ; Refresh
-                                            14400      ; Retry
-                                            3600000    ; Expire
-                                            86400 )    ; Minimum
-                    IN      NS      localhost.
-
-      1       IN      PTR     localhost.
     EOS
   end
 
@@ -196,8 +112,8 @@ class Bind < Formula
         <array>
           <string>#{opt_sbin}/named</string>
           <string>-f</string>
-          <string>-c</string>
-          <string>#{etc}/named.conf</string>
+          <string>-L</string>
+          <string>#{var}/log/named/named.log</string>
         </array>
         <key>ServiceIPC</key>
         <false/>
