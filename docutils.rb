5d4315dfc7c9b6b910775431dad543e40d346cac
Floydian-dk <webmaster@floyd.dk>
2023-02-13 08:54:40 +0100

    node.js: Adding more local formulas

---
+++ docutils.rb
@@ -0,0 +1,23 @@
+class Docutils < Formula
+  include Language::Python::Virtualenv
+
+  desc "Text processing system for reStructuredText"
+  homepage "https://docutils.sourceforge.io"
+  url "https://downloads.sourceforge.net/project/docutils/docutils/0.19/docutils-0.19.tar.gz"
+  sha256 "33995a6753c30b7f577febfc2c50411fec6aac7f7ffeb7c4cfe5991072dcf9e6"
+  license all_of: [:public_domain, "BSD-2-Clause", "GPL-3.0-or-later", "Python-2.0"]
+
+  depends_on "floydian-dk/repo/python@3.10"
+
+  def install
+    virtualenv_install_with_resources
+
+    Dir.glob("#{libexec}/bin/*.py") do |f|
+      bin.install_symlink f => File.basename(f, ".py")
+    end
+  end
+
+  test do
+    system "#{bin}/rst2man.py", "#{prefix}/HISTORY.txt"
+  end
+end
